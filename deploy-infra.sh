f533caeda72686af3615b539e7819193bbd5bbb0
Chris Black <cblack2008@gmail.com>
2024-01-10 19:27:07 -0600

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,6 +6,21 @@ CLI_PROFILE=Dev/DevAdmin
 
 EC2_INSTANCE_TYPE=t2.micro 
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile Dev/DevAdmin \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 echo -e "Using profile [$CLI_PROFILE] and region [$REGION] an instance type of [$EC2_INSTANCE_TYPE]"
